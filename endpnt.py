727644de2825e09b8ce423a4a8fe41dddb472a2a
Aneeq-Younas <85731812+Aneeq-Younas@users.noreply.github.com>
2021-09-20 20:57:47 -0700

    Add files via upload

---
+++ endpnt.py
@@ -0,0 +1,18 @@
+
+print("This is a calculation of endpoint of a line: \n ")
+
+a= int(input("Enter the first value: \n "))
+
+c= int(input("Enter the second value: \n "))
+
+b= int(input("Enter the third value: \n "))
+
+d= int(input("Enter the forth value: \n "))
+
+x= a + (a - b) / 2
+y= b + (b - d) / 2
+
+
+print( "The endpoint first value is: \n ", x)
+print( "The endpoint second  value is: \n ", y)
+
